4e8bd896fed5b5556e383f5c5d1b869a6dc419b5
Rogel Ramas Gerodiaz <gerodiazrogel0@yahoo.com>
2026-04-14 14:23:28 +0800

    clean up unused Carousel component code in Content and format personal links

--- src/constants/personal.ts
+++ src/constants/personal.ts
@@ -1,4 +1,5 @@
 export const personalLinks = {
-  resume: "https://drive.google.com/file/d/1ETmpTnJlS6n7cod75u3gIigSsF01_P9L/view?usp=drive_link", // Replace with your actual resume URL
-  email: "mailto:gerodiazrogel0@gmail.com", 
+  resume:
+    "https://drive.google.com/file/d/1ETmpTnJlS6n7cod75u3gIigSsF01_P9L/view?usp=drive_link",
+  email: "mailto:gerodiazrogel0@gmail.com",
 };
